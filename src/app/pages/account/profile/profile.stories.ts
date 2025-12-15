57dfe32228400accec4912e1a33ae3025a74bcc8
Super Abel <superabel000@gmail.com>
2025-12-15 18:24:18 +0100

    Add profile story

---
+++ src/app/pages/account/profile/profile.stories.ts
@@ -0,0 +1,25 @@
+import { Meta, StoryObj, applicationConfig } from '@storybook/angular';
+import { ProfileComponent } from './profile.component';
+import { Store } from '../../../core/store/store';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+
+const meta: Meta<ProfileComponent> = {
+  title: 'Pages/Profile',
+  component: ProfileComponent,
+  tags: ['autodocs'],
+  decorators: [
+    applicationConfig({
+      providers: [
+        provideHttpClient(),
+        provideRouter([]),
+        Store,
+      ],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<ProfileComponent>;
+
+export const Default: Story = {};
