15fd1cc2309fbf1c6afc6fd3ca5f8c49ccfe1697
Super Abel <superabel000@gmail.com>
2025-12-15 18:24:04 +0100

    Add wishlist button story

---
+++ src/app/components/wishlist-button/wishlist-button.stories.ts
@@ -0,0 +1,34 @@
+import { Meta, StoryObj, applicationConfig } from '@storybook/angular';
+import { WishlistButtonComponent } from './wishlist-button.component';
+import { Store } from '../../core/store/store';
+import { userReducer } from '../../state/user/user.reducer';
+import { provideHttpClient } from '@angular/common/http';
+
+const meta: Meta<WishlistButtonComponent> = {
+  title: 'Components/WishlistButton',
+  component: WishlistButtonComponent,
+  tags: ['autodocs'],
+  decorators: [
+    applicationConfig({
+      providers: [
+        provideHttpClient(),
+        Store,
+      ],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<WishlistButtonComponent>;
+
+export const NotInWishlist: Story = {
+  args: {
+    productId: '1',
+  },
+};
+
+export const InWishlist: Story = {
+  args: {
+    productId: '2',
+  },
+};
