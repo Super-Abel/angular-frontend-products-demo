b75698d3afceb1779748dbe3beff46e920e5d154
Super Abel <superabel000@gmail.com>
2025-12-15 17:43:48 +0100

    Fix wishlist button productId type

---
+++ src/app/components/wishlist-button/wishlist-button.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Store } from '../../core/store/store';
+import { isInWishlist } from '../../state/user/user.selectors';
+import { toggleWishlist } from '../../state/user/user.actions';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-wishlist-button',
+  standalone: true,
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <button
+      (click)="toggle()"
+      [class.text-red-500]="isInWishlist$ | async"
+      [class.text-gray-400]="!(isInWishlist$ | async)"
+      class="text-2xl hover:scale-110 transition"
+      attr.aria-label="{{ (isInWishlist$ | async) ? 'Retirer de' : 'Ajouter à' }} la wishlist"
+    >
+      {{ (isInWishlist$ | async) ? '❤' : '♡' }}
+    </button>
+  `,
+})
+export class WishlistButtonComponent {
+  @Input() productId!: number;
+  isInWishlist$!: Observable<boolean>;
+
+  constructor(private store: Store<any>) {}
+
+  ngOnInit() {
+    this.isInWishlist$ = isInWishlist(this.productId)(this.store.getState$());
+  }
+
+  toggle() {
+    this.store.dispatch(toggleWishlist(this.productId));
+  }
+}
