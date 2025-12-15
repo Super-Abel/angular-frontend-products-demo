a5863be7f9ebbf867ce9638f78e91c1c499b9dc9
Super Abel <superabel000@gmail.com>
2025-12-15 18:23:39 +0100

    Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home.component';
 import { DevIndexComponent } from './dev/dev-index.component';
 import { DevAuthComponent } from './dev/dev-auth.component';
 import { DevProductsComponent } from './dev/dev-products.component';
@@ -7,22 +6,40 @@ import { DevProductRatingComponent } from './dev/dev-product-rating.component';
 import { AppPlaceholderComponent } from './app-placeholder.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ProductsComponent } from './pages/products/products.component';
-import { ProductRatingComponent } from './pages/product-rating/product-rating.component';
 import { Cart } from './pages/cart/cart';
 import { Checkout } from './pages/checkout/checkout';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'dev', component: DevIndexComponent },
   { path: 'dev/auth', component: DevAuthComponent },
   { path: 'dev/products', component: DevProductsComponent },
   { path: 'dev/products/:id/rating', component: DevProductRatingComponent },
   { path: 'app', component: AppPlaceholderComponent },
   { path: 'login', component: LoginComponent },
+
+  // Shop module (lazy)
+  {
+    path: 'shop',
+    loadChildren: () => import('./modules/shop/shop.routes').then((m) => m.SHOP_ROUTES),
+  },
+
+  // Account module (lazy)
+  {
+    path: 'account',
+    loadChildren: () => import('./modules/account/account.routes').then((m) => m.ACCOUNT_ROUTES),
+  },
+
+  // Admin module (lazy)
+  {
+    path: 'admin',
+    loadChildren: () => import('./modules/admin/admin.routes').then((m) => m.ADMIN_ROUTES),
+  },
+
+  // Legacy routes (backward compat)
   { path: 'products', component: ProductsComponent },
-  { path: 'shop/products', component: ProductsComponent },
-  { path: 'shop/rating', component: ProductRatingComponent },
   { path: 'cart', component: Cart },
   { path: 'checkout', component: Checkout },
+
   { path: '**', redirectTo: '' },
 ];
